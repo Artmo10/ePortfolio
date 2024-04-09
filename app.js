03f708748abf5beffcb156dc62db0df9753b7de6
Arturo Morales <arturomorales@Arturos-MBP.hitronhub.home>
2024-04-09 19:46:32 -0400

    contact form loading and success message state added

---
+++ app.js
@@ -0,0 +1,33 @@
+function openMenu() { 
+    const body = document.body;
+
+    body.classList.add("open")
+}
+
+function closeMenu() { 
+    const body = document.body;
+
+    body.classList.remove("open");
+}
+
+// template_y61wr1u
+// service_rp9zf5l
+// nkkaCcHuk9QNnD2oo
+
+async function sendEmail(event) { 
+    event.preventDefault();
+    const body = document.body;
+    const loading = document.querySelector(".contact_form_loading");
+    
+    // Loading State
+    loading.classList.remove("hidden");
+    
+    await emailjs.sendForm("service_rp9zf5l", "template_y61wr1u", event.target, "nkkaCcHuk9QNnD2oo");
+
+    // Success State
+    loading.classList.add("hidden");
+    body.classList.add("success-open");
+    setTimeout(() => { 
+        body.classList.remove("success-open");
+    }, 5000);
+}
