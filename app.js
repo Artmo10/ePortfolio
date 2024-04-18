4c4fac08c46af89b00cda771b15a87f9a0576af8
Arturo Morales <arturomorales@Arturos-MBP.hitronhub.home>
2024-04-17 20:50:13 -0400

    minor changes and simple animation added

--- app.js
+++ app.js
@@ -18,16 +18,25 @@ async function sendEmail(event) {
     event.preventDefault();
     const body = document.body;
     const loading = document.querySelector(".contact_form_loading");
-    
-    // Loading State
-    loading.classList.remove("hidden");
-    
-    await emailjs.sendForm("service_rp9zf5l", "template_y61wr1u", event.target, "nkkaCcHuk9QNnD2oo");
+    const form = document.querySelector(".contact_form");
 
-    // Success State
-    loading.classList.add("hidden");
-    body.classList.add("success-open");
-    setTimeout(() => { 
-        body.classList.remove("success-open");
-    }, 5000);
+    try {
+        //throw new Error();
+        // Loading State
+        loading.classList.remove("hidden");
+        
+        await emailjs.sendForm("service_rp9zf5l", "template_y61wr1u", event.target, "nkkaCcHuk9QNnD2oo");
+
+        // Success State
+        form.reset();
+        loading.classList.add("hidden");
+        body.classList.add("success-open");
+        setTimeout(() => { 
+            body.classList.remove("success-open");
+        }, 4000);
+    } catch { 
+        loading.classList.add("hidden");
+        alert("Error sending email. Please try again later or contact me at arturo.morales.veliz@gmail.com");
+    }
+    
 }
